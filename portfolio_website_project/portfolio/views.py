8fce428ad43d1094674186c252151c576095e6ba
chandrakant salutagi <scsalutagi@gmail.com>
2020-08-09 12:37:58 +0530

    display all projects in a template

--- portfolio_website_project/portfolio/views.py
+++ portfolio_website_project/portfolio/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from . import models
+
 # Create your views here.
 def home(request):
 
-    context = {}
+    projects = models.Project.objects.all()
+
+    print(projects)
+
+    context = {'projects': projects}
 
     return render(request, 'portfolio/index.html', context=context)
