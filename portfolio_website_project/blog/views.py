f2cd5ca23d89c5fe0893e9d755376df29097ebec
chandrakant salutagi <scsalutagi@gmail.com>
2020-08-09 12:59:25 +0530

    Add model for blog

--- portfolio_website_project/blog/views.py
+++ portfolio_website_project/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
+from . import models
+
 # Create your views here.
 
 def all_blogs_view(request):
+    blogs = models.Blog.objects.all()
 
-    context = {}
+    context = {'blogs': blogs}
     return render(request, 'blog/all_blogs.html', context=context)
