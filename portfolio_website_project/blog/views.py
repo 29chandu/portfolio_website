d1b9f682e22c7739b1fcfd33ae78f6cb93225f56
chandrakant salutagi <scsalutagi@gmail.com>
2020-08-09 12:43:57 +0530

    Add base template for all_blogs

--- portfolio_website_project/blog/views.py
+++ portfolio_website_project/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def all_blogs_view(request):
+
+    context = {}
+    return render(request, 'blog/all_blogs.html', context=context)
